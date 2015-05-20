a124750c5d1ef004198d95374656e3ca217e8917
Kyle Leng <hyle200@gmail.com>
2015-05-20 13:36:20 -0400

    Another Typo

--- Assignment-1/js/game.js
+++ Assignment-1/js/game.js
@@ -37,7 +37,7 @@ var hero = {
 };
 var monster = {};
 
-if (localStorage.getItem("monstersKilled")){ //if they have played the game before continue from where they last left off
+if (localStorage.getItem("monstersCaught")){ //if they have played the game before continue from where they last left off
 	var monstersCaught = Number(localStorage.getItem("monstersCaught"));
 }else{ //if they haven't played the game before start the monster count at 0
 	var monstersCaught = 0;
